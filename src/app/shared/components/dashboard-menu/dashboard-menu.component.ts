3ffd502cb7bb069ae98184865c7676ca81680854
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-03-08 23:12:48 -0300

    dashboard: finish styling | no responsiveness

---
+++ src/app/shared/components/dashboard-menu/dashboard-menu.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-menu',
+  templateUrl: './dashboard-menu.component.html',
+  styleUrls: ['./dashboard-menu.component.scss'],
+})
+export class DashboardMenuComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SubmitButtonFieldComponent } from './components/submit-button-field/submitButton-field.component';
+import { DashboardMenuComponent } from './components/dashboard-menu/dashboard-menu.component';
 
 @NgModule({
   declarations: [
     SubmitButtonFieldComponent,
+    DashboardMenuComponent,
   ],
   imports: [
     CommonModule,
@@ -13,6 +15,7 @@ import { SubmitButtonFieldComponent } from './components/submit-button-field/sub
   ],
   exports: [
     SubmitButtonFieldComponent,
+    DashboardMenuComponent,
   ],
 })
 export class SharedModule { }
